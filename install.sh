35bfa63d6c12f3a0047b290ede8dc9b6a6f16a33
acristo <a_cristo@pvs.uva.es>
2019-12-23 14:11:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -131,28 +131,6 @@ curl https://rclone.org/install.sh | sudo bash
 
 
 
-
-
-#### try to remove the following from the computer...
-#sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
-#This command just added a repository, so you will have to tell your system to trust it. For that, use the following commands:
-#sudo nano /etc/apt/sources.list.d/d-apt.list
-#Now after the “deb” part of the first line, insert this:
-#[trusted=yes]
-sudo aptitude update && sudo aptitude -y install  d-apt-keyring
-sudo aptitude update && sudo aptitude install dmd-compiler dub
-sudo aptitude install onedrive
-## Configuration
-# After the program launch, it will download all of your files to a directory named OneDrive, which is created in your home directory. This section is only for those who want to change the destination directory or add exceptions to the files that get synced.
-# Enter the following commands to create a configuration file for OneDrive:
-mkdir -p ~/.config/onedrive
-cp ~/onedrive/config ~/.config/onedrive/config
-# And now edit it:
-nano ~/.config/onedrive/config
-#### try to remove the previous from the computer...
-
-
-
 ###### pdf viwers and utilities
 # pdf studio viewer
 wget https://download.qoppa.com/pdfstudioviewer/PDFStudioViewer_linux64.sh
@@ -173,9 +151,6 @@ sudo aptitude install signal-desktop -y
 
 
 
-
-
-
 ########### java
 aptitude install -y openjdk-8-jre
 
@@ -187,44 +162,32 @@ alias fiji="~/Fiji.app/ImageJ-linux64"
 
 
 
-
-
 ########### sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-aptitude update
-aptitude install sublime-text
-
-
-
-
+sudo aptitude update && sudo aptitude install sublime-text -y
 
 
 ############ install firefox esr
-aptitude remove firefox
-add-apt-repository ppa:mozillateam/ppa
-aptitude update
-aptitude install firefox-esr
-
-
-
-
-
+sudo aptitude remove firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+sudo aptitude update
+sudo aptitude install firefox-esr
 
 
 ########################## virtualbox
 add-apt-repository 'deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib'
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-aptitude update
-aptitude install virtualbox-6.0
+sudo aptitude update
+sudo aptitude install virtualbox-6.0
 ####### firmar virtual box modules
 # https://slimbook.es/tutoriales/linux/364-firmando-modulo-virtualbox-en-secureboot-uefi-solucion-a-kernel-driver-not-installed-rc-1908
 # https://gist.github.com/kiasaki/5f4f491201d621d93a7d719f46e04009
 modprobe vboxdrv
-aptitude update
-aptitude upgrade
-aptitude install mokutil
+sudo aptitude update
+sudo aptitude upgrade
+sudo aptitude install mokutil
 # Create signing keys
 openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=Virt Box Key/"
 # Sign the module (vboxdrv for this example)
@@ -238,25 +201,21 @@ sudo mokutil --import MOK.der
 # Por último, reiniciamos el ordenador. Nos aparecerá una pantalla azul con una espera de teclado, pulsa la tecla que te pide para interrumpir el arranque.
 
 
-
-
 ######## fonts intall
 # https://www.ostechnix.com/install-microsoft-windows-fonts-ubuntu-16-04/
-aptitude install ttf-mscorefonts-installer
+sudo aptitude install ttf-mscorefonts-installer -y
 fc-cache -f -v
-
-
 
 
 ############### Video editor
 # https://itsfoss.com/best-video-editing-software-linux/#blender
 ### Installing VidCutter
-add-apt-repository ppa:ozmartian/apps
-aptitude update
-aptitude install vidcutter
+sudo add-apt-repository ppa:ozmartian/apps
+sudo aptitude update
+sudo aptitude install vidcutter
 ### Installing OpenShot
 # OpenShot is also available in the repositories of all major Linux distributions. You can simply search for it in your software center. You can also get it from its official website.
-aptitutde install openshot
+sudo aptitutde install openshot
 # https://www.blender.org/thanks/
 ### Installing Kdenlive
 # Kdenlive is available for all major Linux distributions. You can simply search for it in your software center. Various packages are available in the download section of Kdenlive website.
@@ -265,22 +224,20 @@ sudo aptitude install kdenlive
 ######### video screen recorder
 # https://www.linuxadictos.com/grabar-pantalla.html
 ## vokoscreen
-sudo aptitude update
-sudo aptitude install vokoscreen 
+sudo aptitude update && sudo aptitude install vokoscreen  -y
 
 # text mdb utility
 # https://bioinf.shenwei.me/csvtk/
 ## https://github.com/shenwei356/csvtk/releases
 wget https://github.com/shenwei356/csvtk/releases/download/v0.19.1/csvtk_linux_amd64.tar.gz
 tar -zxvf *.tar.gz
-cp csvtk /usr/local/bin/
+sudo cp csvtk /usr/local/bin/
 
 
 ## to prevent aye suffering
 # https://www.ostechnix.com/safeeyes-an-useful-linux-utility-that-prevents-eye-strain/
 sudo add-apt-repository ppa:slgobinath/safeeyes
-sudo aptitude update
-sudo aptitude install safeeyes
+sudo aptitude update && sudo aptitude install safeeyes -y
 
 ## to adjust monitor brigthness
 https://www.ostechnix.com/how-to-adjust-monitor-brightness-from-command-line-in-linux/
