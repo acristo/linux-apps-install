c7f3e4d035117dc49fcee098938237e948719407
acristo <a_cristo@pvs.uva.es>
2020-07-04 14:02:04 +0200

    onedrive update

--- install_cloud.sh
+++ install_cloud.sh
@@ -16,8 +16,8 @@ sudo dpkg -i ~/Downloads/megasync-xUbuntu_18.04_amd64.deb
 
 ### ocamlfuse
 sudo add-apt-repository ppa:alessandro-strada/ppa
-sudo apt-get update
-sudo apt-get install google-drive-ocamlfuse
+sudo aptitude update
+sudo aptitude install google-drive-ocamlfuse
 google-drive-ocamlfuse
 mkdir ~/google-drive
 google-drive-ocamlfuse ~/google-drive
@@ -34,7 +34,7 @@ sudo add-apt-repository 'deb-src http://ppa.launchpad.net/nextcloud-devs/client/
 sudo add-apt-repository ppa:nextcloud-devs/client
 #Signing key:    4096R/1FCD77DD0DBEF5699AD2610160EE47FBAD3DD469 (What is this?) 
 #Fingerprint:    1FCD77DD0DBEF5699AD2610160EE47FBAD3DD469 
-sudo apt-get update
+sudo aptitude update
 sudo aptitude install -y nextcloud-client
 
 ###### One cloud client for linux
@@ -59,3 +59,92 @@ cp ~/onedrive/config ~/.config/onedrive/config
 # And now edit it:
 sudo nano ~/.config/onedrive/config
 #### try to remove the previous from the computer...
+
+
+### https://linuxnewbieguide.org/onedrive-client-linux/
+
+sudo aptitude install libcurl4-openssl-dev curl git
+sudo aptitude install libsqlite3-dev
+curl -fsS https://dlang.org/install.sh | bash -s dmd
+source ~/dlang/dmd-2.076.0/activate (this number will vary depending upon the version of dmd, note the output of the dmd build command)
+
+git clone https://github.com/skilion/onedrive.git
+cd onedrive
+
+make
+sudo -s
+source ~/dlang/dmd-2.076.0/activate
+make install
+### do not works
+
+### https://www.fosslinux.com/8835/how-to-sync-microsoft-onedrive-with-linux.htm
+sudo apt install libcurl4-openssl-dev git
+sudo apt install libsqlite3-dev
+sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
+# This command just added a repository, so you will have to tell your system to trust it. For that, use the following commands:
+
+sudo nano /etc/apt/sources.list.d/d-apt.list
+
+Now after the “deb” part of the first line, insert this:
+
+[trusted=yes]
+
+sudo aptitude update && sudo aptitude -y install  d-apt-keyring
+sudo aptitude update && sudo aptitude install dmd-compiler dub
+### do not works
+
+
+### sync one-drive with RClone ######################################################
+#### https://itsfoss.com/use-onedrive-linux-rclone/
+############### Sync Microsoft OneDrive in Linux with rclone ########################
+
+# Using Rclone in Linux is not that complicated but requires some patience and familiarity with the Linux terminal. You need to tweak the configuration a little to make it work. Let’s see how to do that.
+
+### Step 1: Install Rclone
+# I am using Ubuntu 20.04 in this tutorial but you should be able to follow this tutorial in pretty much any Linux distribution. Just the rclone installation instruction could be different but the rest of steps remains the same.
+# In Debian/Ubuntu based distributions use:
+sudo aptitude install rclone
+
+### Step 2: Adding new remote
+# Once you have installed rclone successfully, you need to configure rclone. Enter the following command in the terminal:
+rclone config
+
+### Step 3: Select cloud service you want to sync with rclone
+# After entering name and hitting enter, you will see a list of cloud services like Google cloud storage, Box, One Drive and others.
+# You have to enter the number of the service you want to use. In this case, it’s ‘One Drive’. Make sure you enter the correct number.
+
+### Step 4: Login to OneDrive account
+# When you enter ‘y’ and hit enter, your default browser will open and here you have to log into your Microsoft account. And if it asks for permission click on ‘yes’.
+
+### Step 5: Enter account type
+# Now you have to select account type. For most of the users it will be the first one, ‘One drive Personal or business’. I believe it is personal so go with 1.
+
+### Step 5: Mounting OneDrive int file manager
+# Create folder in your home directory where you will mount OneDrive. I will name the folder “OneDrive”. You can name it whatever you want, but please make sure you change the name to yours in the commands.
+# Create a new folder with mkdir command in your home directory or wherever you want:
+
+mkdir ~/OneDrive
+
+# Now you have to use the following command:
+
+rclone --vfs-cache-mode writes mount "onedrive:Disk.IUGFS":  ~/Disk.IUGFS
+
+# In above command “one drive” is the name of the “remote”, so you should use the correct name there if yours is different. You can check the name of the “remote” in step 2 of this tutorial.
+# This command will mount one drive in given location and will continue to run in terminal. When you stop the process with,ctrl + c the one drive will be unmounted.
+# To mount one drive on startup, follow the next step below.
+
+### Step 6: Mount One Drive on startup
+# Every Linux distribution gives some way to manage startup application. I am using Ubuntu’s Startup Application Preferences tool here.
+# Open “Startup Applications“. And click on “Add“. Now, in the command field, enter the following:
+
+sh -c "rclone --vfs-cache-mode writes mount \"onedrive:Disk.IUGFS\": ~/Disk.IUGFS"
+
+## https://rclone.org/onedrive/ ## manual for use
+# List directories in top level of your OneDrive
+rclone lsd onedrive:
+
+# List all the files in your OneDrive
+rclone ls onedrive:
+
+# To copy a local directory to an OneDrive directory called backup
+rclone copy /home/source onedrive:Disk.IUGFS
--- install_qgis.sh
+++ install_qgis.sh
@@ -3,3 +3,20 @@ sudo add-apt-repository 'deb https://qgis.org/ubuntu-ltr bionic main'
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
 sudo aptitude update
 sudo aptitude install -y qgis qgis-plugin-grass
+
+
+### In case of keyserver errors add the qgis.org repository public key to your apt keyring, type:
+# wget -O - https://qgis.org/downloads/qgis-2019.gpg.key | gpg --import
+# gpg --fingerprint 51F523511C7028C3
+
+### Should output:
+### pub   rsa4096 2019-08-08 [SCEA] [expires: 2020-08-08]
+###       8D5A 5B20 3548 E500 4487  DD19 51F5 2351 1C70 28C3
+### uid           [unknown] QGIS Archive Automatic Signing Key (2019) <qgis-developer@lists.osgeo.org>
+
+### After you have verified the fingerprint you can add the key to apt with:
+# gpg --export --armor 51F523511C7028C3 | sudo apt-key add -
+
+### Alternatively you can download the key from a keyserver and add the key to apt in one go (without manual fingerprint verification):
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key 51F523511C7028C3
+
