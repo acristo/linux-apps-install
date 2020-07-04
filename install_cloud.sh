8ac819f6ae9338605b14323189dae98c6ec0bf2f
acristo <a_cristo@pvs.uva.es>
2020-07-04 14:07:39 +0200

    onedrive update

--- install_cloud.sh
+++ install_cloud.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ######################## cloud storage
 
+##############################################################################################################
 ### mega
 sudo aptitude install libc-ares2 
 sudo aptitude install libcrypto++6 
@@ -9,11 +10,13 @@ sudo aptitude install libzen0v5
 #https://mega.nz/sync
 sudo dpkg -i ~/Downloads/megasync-xUbuntu_18.04_amd64.deb
 
+##############################################################################################################
 ### dropbox
 #wget https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2019.02.14_amd64.deb
 #sudo aptitude install -y libpango1.0-0
 #sudo dpkg -i dropbox_2019.02.14_amd64.deb
 
+##############################################################################################################
 ### ocamlfuse
 sudo add-apt-repository ppa:alessandro-strada/ppa
 sudo aptitude update
@@ -23,11 +26,13 @@ mkdir ~/google-drive
 google-drive-ocamlfuse ~/google-drive
 df -h
 
+##############################################################################################################
 ### lrz (Care4C)
 
 #wget https://syncandshare.lrz.de/client_deployment/LRZ_Sync_Share_Latest_amd64.deb
 #sudo dpkg -i LRZ_Sync_Share_Latest_amd64.deb
 
+##############################################################################################################
 ### nextcloud
 sudo add-apt-repository 'deb http://ppa.launchpad.net/nextcloud-devs/client/ubuntu bionic main'
 sudo add-apt-repository 'deb-src http://ppa.launchpad.net/nextcloud-devs/client/ubuntu bionic main' 
@@ -37,63 +42,14 @@ sudo add-apt-repository ppa:nextcloud-devs/client
 sudo aptitude update
 sudo aptitude install -y nextcloud-client
 
+
+##############################################################################################################
 ###### One cloud client for linux
 ## Rclone
 # https://rclone.org/
 curl https://rclone.org/install.sh | sudo bash
 
-#### try to remove the following from the computer...
-#sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
-#This command just added a repository, so you will have to tell your system to trust it. For that, use the following commands:
-#sudo nano /etc/apt/sources.list.d/d-apt.list
-#Now after the “deb” part of the first line, insert this:
-#[trusted=yes]
-sudo aptitude update && sudo aptitude -y install  d-apt-keyring
-sudo aptitude update && sudo aptitude -y install dmd-compiler dub
-sudo aptitude install onedrive
-## Configuration
-# After the program launch, it will download all of your files to a directory named OneDrive, which is created in your home directory. This section is only for those who want to change the destination directory or add exceptions to the files that get synced.
-# Enter the following commands to create a configuration file for OneDrive:
-mkdir -p ~/.config/onedrive
-cp ~/onedrive/config ~/.config/onedrive/config
-# And now edit it:
-sudo nano ~/.config/onedrive/config
-#### try to remove the previous from the computer...
-
-
-### https://linuxnewbieguide.org/onedrive-client-linux/
-
-sudo aptitude install libcurl4-openssl-dev curl git
-sudo aptitude install libsqlite3-dev
-curl -fsS https://dlang.org/install.sh | bash -s dmd
-source ~/dlang/dmd-2.076.0/activate (this number will vary depending upon the version of dmd, note the output of the dmd build command)
-
-git clone https://github.com/skilion/onedrive.git
-cd onedrive
-
-make
-sudo -s
-source ~/dlang/dmd-2.076.0/activate
-make install
-### do not works
-
-### https://www.fosslinux.com/8835/how-to-sync-microsoft-onedrive-with-linux.htm
-sudo apt install libcurl4-openssl-dev git
-sudo apt install libsqlite3-dev
-sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
-# This command just added a repository, so you will have to tell your system to trust it. For that, use the following commands:
-
-sudo nano /etc/apt/sources.list.d/d-apt.list
-
-Now after the “deb” part of the first line, insert this:
-
-[trusted=yes]
-
-sudo aptitude update && sudo aptitude -y install  d-apt-keyring
-sudo aptitude update && sudo aptitude install dmd-compiler dub
-### do not works
-
-
+##############################################################################################################
 ### sync one-drive with RClone ######################################################
 #### https://itsfoss.com/use-onedrive-linux-rclone/
 ############### Sync Microsoft OneDrive in Linux with rclone ########################
@@ -139,6 +95,8 @@ rclone --vfs-cache-mode writes mount "onedrive:Disk.IUGFS":  ~/Disk.IUGFS
 
 sh -c "rclone --vfs-cache-mode writes mount \"onedrive:Disk.IUGFS\": ~/Disk.IUGFS"
 
+##############################################################################################################
+##############################################################################################################
 ## https://rclone.org/onedrive/ ## manual for use
 # List directories in top level of your OneDrive
 rclone lsd onedrive:
