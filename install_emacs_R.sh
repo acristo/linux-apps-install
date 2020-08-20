d976326eb4490a9e950002d9bf2231e01135ddcd
acristo <a_cristo@pvs.uva.es>
2020-08-20 10:17:15 +0200

    update R

--- install_emacs_R.sh
+++ install_emacs_R.sh
@@ -7,7 +7,7 @@ sudo aptitude install -y emacs
 
 sudo apt install apt-transport-https software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran40/'
 sudo aptitude update 
 sudo aptitude install -y r-base
 
