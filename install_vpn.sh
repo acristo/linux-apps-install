f9d5099dbc508b403d94f357089cdaac68a2f090
Cristobal Ordoñez <angelcristobal.ordonez@uva.es>
2022-07-27 13:23:25 +0200

    add vpn

---
+++ install_vpn.sh
@@ -0,0 +1,13 @@
+  sudo apt install openfortivpn 
+    sudo vi /etc/openfortivpn/config 
+      sudo openfortivpn 
+
+
+
+      #### contenido del fichero config
+      host = rpv.uva.es
+      port = 443
+      username = e+DNI
+      password = 
+      trusted-cert = cdfdd1ae13e266788b70f8522ace03d7e56cb353945a02e2b7dd6e2d52212025
+
